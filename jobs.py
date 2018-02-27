50ea432b4abf78249e14ac2f3bbaf02f8a56d7f2
Thierry DECKER <mail@thierry-decker.com>
2018-02-27 13:21:43 +0100

    Added host type to every measurement

--- jobs.py
+++ jobs.py
@@ -30,8 +30,8 @@ def cpu_times(influxdb_config):
         {
             "measurement": "cpu_times",
             "tags": {
-                "host": platform.node(),
                 "host_type": host_type,
+                "host_name": platform.node(),
             },
             "fields": {
                 "user": result.user,
@@ -61,8 +61,8 @@ def cpu_times_percpu(influxdb_config):
                 {
                     "measurement": "cpu_times_percpu",
                     "tags": {
-                        "host": platform.node(),
                         "host_type": host_type,
+                        "host_name": platform.node(),
                         "cpu_num": cpu_num,
                     },
                     "fields": {
@@ -82,12 +82,14 @@ def cpu_percent(influxdb_config):
     :return: None
     """
     result = psutil.cpu_percent(interval=.1, percpu=False)
+    host_type = helpers.get_host_type()
     client = helpers.get_influxdb_client(influxdb_config)
     record = [
         {
             "measurement": "cpu_percent",
             "tags": {
-                "host": platform.node(),
+                "host_type": host_type,
+                "host_name": platform.node(),
             },
             "fields": {
                 "current": result,
@@ -104,6 +106,7 @@ def cpu_percent_percpu(influxdb_config):
     :return: None
     """
     results = psutil.cpu_percent(interval=.1, percpu=True)
+    host_type = helpers.get_host_type()
     client = helpers.get_influxdb_client(influxdb_config)
     records = []
     for cpu_num, result in enumerate(results):
@@ -111,7 +114,8 @@ def cpu_percent_percpu(influxdb_config):
                 {
                     "measurement": "cpu_percent_percpu",
                     "tags": {
-                        "host": platform.node(),
+                        "host_type": host_type,
+                        "host_name": platform.node(),
                         "cpu_num": cpu_num,
                     },
                     "fields": {
@@ -129,12 +133,14 @@ def cpu_times_percent(influxdb_config):
     :return: None
     """
     result = psutil.cpu_times_percent(interval=0.1, percpu=False)
+    host_type = helpers.get_host_type()
     client = helpers.get_influxdb_client(influxdb_config)
     record = [
         {
             "measurement": "cpu_times_percent",
             "tags": {
-                "host": platform.node(),
+                "host_type": host_type,
+                "host_name": platform.node(),
             },
             "fields": {
                 "user": result.user,
@@ -153,6 +159,7 @@ def cpu_times_percent_percpu(influxdb_config):
     :return: None
     """
     results = psutil.cpu_times_percent(interval=.1, percpu=True)
+    host_type = helpers.get_host_type()
     client = helpers.get_influxdb_client(influxdb_config)
     records = []
     for cpu_num, result in enumerate(results):
@@ -160,7 +167,8 @@ def cpu_times_percent_percpu(influxdb_config):
                 {
                     "measurement": "cpu_times_percent_percpu",
                     "tags": {
-                        "host": platform.node(),
+                        "host_type": host_type,
+                        "host_name": platform.node(),
                         "cpu_num": cpu_num,
                     },
                     "fields": {
@@ -180,12 +188,14 @@ def cpu_count(influxdb_config):
     :return: None
     """
     result = psutil.cpu_count()
+    host_type = helpers.get_host_type()
     client = helpers.get_influxdb_client(influxdb_config)
     record = [
         {
             "measurement": "cpu_count",
             "tags": {
-                "host": platform.node(),
+                "host_type": host_type,
+                "host_name": platform.node(),
             },
             "fields": {
                 "value": result,
@@ -202,12 +212,14 @@ def cpu_stats(influxdb_config):
     :return: None
     """
     result = psutil.cpu_stats()
+    host_type = helpers.get_host_type()
     client = helpers.get_influxdb_client(influxdb_config)
     record = [
         {
             "measurement": "cpu_stats",
             "tags": {
-                "host": platform.node(),
+                "host_type": host_type,
+                "host_name": platform.node(),
             },
             "fields": {
                 "ctx_switches": result.ctx_switches,
@@ -227,12 +239,14 @@ def cpu_freq(influxdb_config):
     :return: None
     """
     result = psutil.cpu_freq(percpu=False)
+    host_type = helpers.get_host_type()
     client = helpers.get_influxdb_client(influxdb_config)
     record = [
         {
             "measurement": "cpu_freq",
             "tags": {
-                "host": platform.node(),
+                "host_type": host_type,
+                "host_name": platform.node(),
             },
             "fields": {
                 "current": result.current,
@@ -251,6 +265,7 @@ def cpu_freq_percpu(influxdb_config):
     :return: None
     """
     results = psutil.cpu_freq(percpu=True)
+    host_type = helpers.get_host_type()
     client = helpers.get_influxdb_client(influxdb_config)
     records = []
     for cpu_num, result in enumerate(results):
@@ -258,7 +273,8 @@ def cpu_freq_percpu(influxdb_config):
                 {
                     "measurement": "cpu_freq_percpu",
                     "tags": {
-                        "host": platform.node(),
+                        "host_type": host_type,
+                        "host_name": platform.node(),
                         "cpu_num": cpu_num,
                     },
                     "fields": {
