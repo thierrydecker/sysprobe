15822744682ed782416fd444be78b4a9db9bfa46
Thierry DECKER <mail@thierry-decker.com>
2018-02-28 06:38:34 +0100

    Added virtual_memory() in jobsmemory.py

---
+++ jobsmemory.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+#
+# Standard library imports
+#
+import platform
+#
+# Third party imports
+#
+import psutil
+#
+# Project's imports
+#
+import helpers
+
+
+def virtual_memory(influxdb_config):
+    """Retrieve the system memory usage
+
+    :param influxdb_config: A dictionary of InfluxDB to connect to
+    :return: None
+    """
+    result = psutil.virtual_memory()
+    host_type = helpers.get_host_type()
+    #
+    # TODO -> Add platform specific counters
+    #
+    client = helpers.get_influxdb_client(influxdb_config)
+    record = [
+        {
+            "measurement": "virtual_memory",
+            "tags": {
+                "host_type": host_type,
+                "host_name": platform.node(),
+            },
+            "fields": {
+                "used": result.used,
+                "free": result.free,
+            }
+        }
+    ]
+    helpers.influxdb_write_points(client, record)
